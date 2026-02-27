236163c3299326408917865661bccf08511ec98e
Olajide9909 <spiritlifeschoolofministry@gmail.com>
2026-02-27 13:58:18 +0100

    SUPABASE SERVICE ROLE KEY SUPABASE SERVICE ROLE KEY

--- supabase/functions/send-registration-email/index.ts
+++ supabase/functions/send-registration-email/index.ts
@@ -7,7 +7,7 @@ serve(async (req) => {
 
     const supabase = createClient(
       Deno.env.get('SUPABASE_URL')!,
-      Deno.env.get('SERVICE_ROLE_KEY')!
+      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!
     )
 
     const { data: profile } = await supabase
